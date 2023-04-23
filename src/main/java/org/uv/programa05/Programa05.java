bc4df4a902eb3a2fcedf0f439b1e844d4522cd8e
erick <erikc_48@hotmail.com>
2023-04-23 17:15:08 -0600

    programa 05

---
+++ src/main/java/org/uv/programa05/Programa05.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.uv.programa05;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author erikc
+ */
+public class Programa05 {
+
+    public static void main(String[] args) {
+//        Session session = HibernateUtil.getSession();
+//        DAOEmpleado dao = new DAOEmpleado();
+//        Empleado empleado = new Empleado();
+//        empleado.setClave(11);
+//        empleado.setNombre("Erick");
+//        empleado.setDireccion("av 20");
+//        empleado.setTelefono("123456789");
+//        
+//        dao.create(empleado);
+        
+        System.out.println("Hello World!");
+    }
+}
